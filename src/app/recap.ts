d60412a6300529797cf6c6c4cf284b2309a6de5f
Fernando Robles <ferroblesh@gmail.com>
2021-09-28 22:11:00 -0500

    basic typescript

---
+++ src/app/recap.ts
@@ -0,0 +1,13 @@
+const username: string = 'ferroblesh';
+const sum = (a: number, b: number) => {
+  return a+b;
+}
+sum(1,2);
+
+class Person {
+  constructor(public age: number,public lastname: string) {
+  }
+}
+
+const fer = new Person(34, 'Robles');
+
