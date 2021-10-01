af27c1b2bbe274e8e28ca5823b616d55decbe4e2
Fernando Robles <ferroblesh@gmail.com>
2021-09-30 22:08:22 -0500

    class & style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Product } from './product.model'
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  widthImg=10;
   name = 'Fer';
   age = 18;
   img = 'https://source.unsplash.com/random';
